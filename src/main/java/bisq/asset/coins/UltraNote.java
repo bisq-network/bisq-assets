0c6c287df87244deb5b884e377630a14bbbb1bdc
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2018-08-13 15:42:53 +0200

    Apply bisq code style

--- src/main/java/bisq/asset/coins/UltraNote.java
+++ src/main/java/bisq/asset/coins/UltraNote.java
@@ -1,6 +1,8 @@
 package bisq.asset.coins;
+
 import bisq.asset.Coin;
 import bisq.asset.RegexAddressValidator;
+
 public class UltraNote extends Coin {
     public UltraNote() {
         super("UltraNote", "XUN", new RegexAddressValidator("(Xun3).{94}"));
